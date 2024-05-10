d2472695fc2d2df2a1f8ae83900002bcad4fb70b
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-05-10 17:23:23 +0300

    Created methods of arithmetic operations

--- src/main/java/com/calculator/goryachev/calculator/CalculatorController.java
+++ src/main/java/com/calculator/goryachev/calculator/CalculatorController.java
@@ -1,6 +1,7 @@
 package com.calculator.goryachev.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,26 @@ public class CalculatorController {
     @GetMapping(path = "/calculator")
     public String answerWelcome() {
         return "Добро пожаловать в калькулятор.";
+    }
+
+    @GetMapping(path = "/calculator/plus")
+    public String plusNumbers(@RequestParam int num1, @RequestParam int num2) {
+        int sum = num1 + num2;
+        return num1 + " + " + num2 + " = " + sum;
+    }
+    @GetMapping(path = "/calculator/minus")
+    public String minusNumbers(@RequestParam int num1, @RequestParam int num2) {
+        int sum = num1 - num2;
+        return num1 + " - " + num2 + " = " + sum;
+    }
+    @GetMapping(path = "/calculator/multiply")
+    public String multiplyNumbers(@RequestParam int num1, @RequestParam int num2) {
+        int sum = num1 * num2;
+        return num1 + " * " + num2 + " = " + sum;
+    }
+    @GetMapping(path = "/calculator/divide")
+    public String divideNumbers(@RequestParam int num1, @RequestParam int num2) {
+        int sum = num1 / num2;
+        return num1 + " / " + num2 + " = " + sum;
     }
 }
