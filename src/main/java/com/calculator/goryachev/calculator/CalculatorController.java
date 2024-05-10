89c749dfbe04b2756f77b2f8947757a918b24490
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-05-10 18:31:00 +0300

    - Created class "CalculatorService". - Moved the code to do the calculations to class "CalculatorService". - Corrected class "CalculatorController". - Wrote code to display an error when specifying numbers for calculation.

--- src/main/java/com/calculator/goryachev/calculator/CalculatorController.java
+++ src/main/java/com/calculator/goryachev/calculator/CalculatorController.java
@@ -1,34 +1,59 @@
 package com.calculator.goryachev.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    @Autowired
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
     @GetMapping(path = "/calculator")
     public String answerWelcome() {
         return "Добро пожаловать в калькулятор.";
     }
 
     @GetMapping(path = "/calculator/plus")
-    public String plusNumbers(@RequestParam int num1, @RequestParam int num2) {
-        int sum = num1 + num2;
-        return num1 + " + " + num2 + " = " + sum;
+    public ResponseEntity<String> plusNumbers(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        if (num1 == null || num2 == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Для выполнения операции необходимо указать два числа.");
+        }
+        int sum = calculatorService.plusNumbers(num1, num2);
+        return ResponseEntity.ok(num1 + " + " + num2 + " = " + sum);
     }
+
     @GetMapping(path = "/calculator/minus")
-    public String minusNumbers(@RequestParam int num1, @RequestParam int num2) {
-        int sum = num1 - num2;
-        return num1 + " - " + num2 + " = " + sum;
+    public ResponseEntity<String> minusNumbers(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        if (num1 == null || num2 == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Для выполнения операции необходимо указать два числа.");
+        }
+        int sum = calculatorService.minusNumbers(num1, num2);
+        return ResponseEntity.ok(num1 + " - " + num2 + " = " + sum);
     }
+
     @GetMapping(path = "/calculator/multiply")
-    public String multiplyNumbers(@RequestParam int num1, @RequestParam int num2) {
-        int sum = num1 * num2;
-        return num1 + " * " + num2 + " = " + sum;
+    public ResponseEntity<String> multiplyNumbers(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        if (num1 == null || num2 == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Для выполнения операции необходимо указать два числа.");
+        }
+        int sum = calculatorService.multiplyNumbers(num1, num2);
+        return ResponseEntity.ok(num1 + " * " + num2 + " = " + sum);
     }
+
     @GetMapping(path = "/calculator/divide")
-    public String divideNumbers(@RequestParam int num1, @RequestParam int num2) {
-        int sum = num1 / num2;
-        return num1 + " / " + num2 + " = " + sum;
+    public ResponseEntity<String> divideNumbers(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        if (num1 == null || num2 == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Для выполнения операции необходимо указать два числа.");
+        }
+        int sum = calculatorService.divideNumbers(num1, num2);
+        return ResponseEntity.ok(num1 + " / " + num2 + " = " + sum);
     }
 }
